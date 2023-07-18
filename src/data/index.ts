c787fa6d97480682a215d4a16aa1dfb0b7556646
Yuliia Panchenko <72158175+J-Panchenko@users.noreply.github.com>
2023-07-18 12:01:12 +0300

    remove mailjet service

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,14 +1,9 @@
 import { contacts, menuList } from './initialData';
-import { error, success } from './userMessage';
 import { externalLinks } from './externalLinks';
 import { recentWork } from './recentWork';
-import { regex } from './regex';
 import { schemaForm } from './schemaForm';
 
 export {
-  regex,
-  error,
-  success,
   menuList,
   schemaForm,
   externalLinks,
